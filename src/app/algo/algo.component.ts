9fb2c7b3d3a3049d0c189d1cf2dd3c1b17cebfcb
devvdevv <61457360+devvdevv@users.noreply.github.com>
2020-08-21 17:45:04 +0700

    add animation when sorting

--- src/app/algo/algo.component.ts
+++ src/app/algo/algo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { element } from 'protractor';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-algo',
@@ -8,7 +9,7 @@ import { element } from 'protractor';
 })
 export class AlgoComponent implements OnInit {
 
-  array : number[] = [100, 300, 234, 453, 123, 80, 222, 341, 113];
+  array: number[] = [100, 300, 234, 453, 123, 80, 222, 341, 113];
 
   constructor() { }
 
@@ -16,17 +17,34 @@ export class AlgoComponent implements OnInit {
     // this.array = [100, 200];
   }
 
-  sort(){
-    for(var i = 0; i < this.array.length; i++) {
-      for(var j = i+1; j < this.array.length; j++) {
-        if(this.array[i] > this.array[j]) {
-          // this.swap(this.array[i], this.array[j]);
+  num1: number = 0;
+  num2: number = 0;
+
+  selectionSort() {
+    this.sort;
+  }
+
+  sort = async () => {
+    for (var i = 0; i < this.array.length; i++) {
+      this.num1 = this.array[i];
+      for (var j = i + 1; j < this.array.length; j++) {
+        this.num2 = this.array[j];
+        await this.sleep(500);
+        if (this.array[i] > this.array[j]) {
           var temp = this.array[i];
           this.array[i] = this.array[j];
           this.array[j] = temp;
+          this.num1 = this.array[i];
+          this.num2 = this.array[j];
+
         }
+
       }
     }
+  }
+
+  sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
   }
 
   swap(num1, num2) {
