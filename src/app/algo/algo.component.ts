84ebc340d2e2a5ef9e7fbe78582cbeb8275303f8
devvdevv <61457360+devvdevv@users.noreply.github.com>
2020-08-21 20:41:39 +0700

    improving visual
    + add color
    + add delay

--- src/app/algo/algo.component.ts
+++ src/app/algo/algo.component.ts
@@ -10,37 +10,48 @@ import { resolve } from 'dns';
 export class AlgoComponent implements OnInit {
 
   array: number[] = [100, 300, 234, 453, 123, 80, 222, 341, 113];
-
-  constructor() { }
-
-  ngOnInit(): void {
-    // this.array = [100, 200];
-  }
+  sortColor: String;
 
   num1: number = 0;
   num2: number = 0;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.sortColor = this.color(0);
+  }
+
+
   selectionSort() {
     this.sort;
+    
   }
 
   sort = async () => {
     for (var i = 0; i < this.array.length; i++) {
       this.num1 = this.array[i];
+      this.sortColor = this.color(1);
       for (var j = i + 1; j < this.array.length; j++) {
         this.num2 = this.array[j];
+        this.sortColor = this.color(1);
         await this.sleep(500);
         if (this.array[i] > this.array[j]) {
+
           var temp = this.array[i];
           this.array[i] = this.array[j];
           this.array[j] = temp;
+
           this.num1 = this.array[i];
           this.num2 = this.array[j];
 
+          this.sortColor = this.color(2);
+
+          await this.sleep(400);
         }
 
       }
     }
+    this.sortColor = this.color(0);
   }
 
   sleep = (milliseconds) => {
@@ -51,6 +62,15 @@ export class AlgoComponent implements OnInit {
     var temp = num1;
     num1 = num2;
     num2 = temp;
+  }
+
+  color(number) {
+    switch(number){
+      case 0: return '#ff4081';
+      case 1: return '#69f0ae';
+      case 2: return '#ffd740';
+      default: return null;
+    }
   }
 
 
