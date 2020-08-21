5728ffa227c0ca97860577c933cc42bd47d4ed97
devvdevv <61457360+devvdevv@users.noreply.github.com>
2020-08-21 09:46:31 +0700

    sorting implement test

--- src/app/algo/algo.component.ts
+++ src/app/algo/algo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-algo',
@@ -7,13 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlgoComponent implements OnInit {
 
-  array: number[];
-
+  array : number[] = [100, 300, 234, 453, 123, 80, 222, 341, 113];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.array = [100, 200];
+    // this.array = [100, 200];
   }
+
+  sort(){
+    for(var i = 0; i < this.array.length; i++) {
+      for(var j = i+1; j < this.array.length; j++) {
+        if(this.array[i] > this.array[j]) {
+          // this.swap(this.array[i], this.array[j]);
+          var temp = this.array[i];
+          this.array[i] = this.array[j];
+          this.array[j] = temp;
+        }
+      }
+    }
+  }
+
+  swap(num1, num2) {
+    var temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
 
 }
